f3f4cf8b1c76411c0ca02db2c196af2b823dee3c
jcnpereira <casimiro@ipt.pt>
2024-11-08 14:18:43 +0000

    versão inicial do projeto

---
+++ AMinhaPrimeiraAplicacao/app/src/main/java/pt/ipt/dam2023/aminhaprimeiraaplicacao/MainActivity.kt
@@ -0,0 +1,14 @@
+package pt.ipt.dam2023.aminhaprimeiraaplicacao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+}
